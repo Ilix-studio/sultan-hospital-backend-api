4e23e6996b1122452df17d1323b09275498b4595
Ilish jyotish <40943217+Ilix-studio@users.noreply.github.com>
2024-08-08 21:36:41 +0530

    no changes

--- server/index.js
+++ server/index.js
@@ -32,16 +32,6 @@ app.use(cookieParser());
 app.use("/api/form", appointmentFormRoute);
 app.use("/api/admin", loginRoutes);
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
 app.use(routeNotFound);
 app.use(errorHandler);
 
