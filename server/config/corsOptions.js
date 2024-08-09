66a565a8aed840901fa94b60eef6c4a9b3d75ab4
Ilish jyotish <40943217+Ilix-studio@users.noreply.github.com>
2024-08-09 11:52:58 +0530

    cors option set again

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,15 @@
+const allowOrigins = [
+  "https://sultanhospital.in",
+  "https://sultanhospital.in/login",
+];
 const corsOptions = {
-  origin: "https://sultanhospital.in/",
+  origin: (origin, callback) => {
+    if (allowOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
